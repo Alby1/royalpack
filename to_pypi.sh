c597badb39dbc57f0695affc606f6f2da6d3af28
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-11-11 10:00:58 +0100

    5.0a92

--- to_pypi.sh
+++ to_pypi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Royalnet must be installed with `develop`
-VERSION=$(python3.7 -m royalnet.version)
+VERSION=$(python3.7 -m royalpack.version)
 
 rm -rf dist
 python setup.py sdist bdist_wheel
-twine upload "dist/royalnet-$VERSION"*
+twine upload "dist/royalpack-$VERSION"*
 git add *
 git commit -m "$VERSION"
 git push
-hub release create --message "Royalnet $VERSION" --prerelease "$VERSION"
+hub release create --message "Version $VERSION" --prerelease "$VERSION"
